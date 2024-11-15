41d0a5454a4d70f8edcd975c48cf57c8d09f581b
Andrejev Denis <andrejevdenis00@gmail.com>
2024-11-15 15:54:58 +0400

    Получилось!

--- test_google_should_find_selene.py
+++ test_google_should_find_selene.py
@@ -2,14 +2,15 @@ from selene import browser, be, have
 import pytest
 
 @pytest.fixture
-def serch_page(openbrowser):
+def serch1(openbrowser):
     browser.element('[name="q"]').should(be.blank).type('налог ру').press_enter()
+
+def test_serch1(serch1):
     browser.element('[id="search"]').should(have.text('Федеральная налоговая служба'))
 
 @pytest.fixture
-def result(serch_page):
-    return serch_page
+def serch2(openbrowser):
+    browser.element('[name="q"]').should(be.blank).type('|\|\|').press_enter()
 
-def test_serch(serch_page, result):
-
-    assert result == "Федеральная налоговая служба"
+def test_serch2(serch2):
+    browser.element('[aria-level="3"]').should(have.text(' ничего не найдено.'))
